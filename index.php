20b5ca46141dcfb3995ec68b0b9ee93586213ea9
Atanasova,Kalina K.D <k.atanasova@student.fontys.nl>
2020-05-06 10:24:15 +0200

    fixed layout

---
+++ index.php
@@ -0,0 +1,306 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/name.jpg">
+    <title>Kalina Atanasova</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/main.css">
+    
+</head>
+
+<body  >
+    <!-- header area -->
+        
+    <div class="container1">
+<nav class="navbar font-size-27 p-3" style="display: flex; justify-content: space-between; align-items: center;width: 100%;">
+    <ul class="" style="" id="menu">
+        <li class="p-0"><a href="#home1" class="font-pl" >Home</a></li>
+        <li class="p-0"><a href="#about" class="font-pl">About</a></li>
+        <li class="p-0"><a href="#skills" class="font-pl">Skills</a></li>
+        <li class="p-0"><a href="#projects"  class="font-pl">Projects</a></li>
+        <li class="p-0"><a href="#illustrations"  class="font-pl">Illustrations</a></li>
+        <li class="p-0"><a href="#contact"  class="font-pl">Contact</a></li>
+    </ul>
+    
+</nav>
+  
+    <div id="site-main" class="" style="color:white;" >
+        
+    <section class="site-banner py-5 " id="home" >
+         <div class="banner-area" >
+            <div id="home1">
+                <div>
+                    <img src="./assets/az.jpg" style="width: 100%;">
+                </div>
+                <div>
+                  <img src="./assets/lg.jpg" style="width:100%"/>
+                </div>
+        </div> 
+    </section>
+   
+     <!-- about me area -->
+     <section id="about" class="about " style="padding: 40px 10%;">
+<div class="me py-5">
+    <h5 class="text-uppercase font-staat font-size-34 ">Who's that girl?</h5> 
+</div>
+<div class="row m-0" style="display: flex; justify-content: space-between;">
+    <div class=" p-0 imageBox" style="width: fit-content;height: fit-content;">
+            <div class="imageInn" style="width: fit-content;">
+              <img src="./assets/az-bw.jpg" alt="Default Image" style="height: 420px;display: block;margin: 0px;padding:0px;">
+            </div>
+            <div class="hoverImg">
+              <img src="./assets/k.jpg" alt="Profile Image" style="height: 420px;display: block;margin-left: auto;margin-right: auto;">
+            </div>
+    </div>
+    <div class="col-sm-7 py-5">
+        <h5><strong>Hello!</strong></h5>
+       
+        <p class="font-inter py-2" >I'm Kalina Atanasova, creative junkie with a curious mind, originally from Bulgaria but currently based in the Netherlands.
+          </p>
+            <p> From a very young age, my passion for arts, crafts, and all things creative was simply undeniable.
+                My interest in programming, however, came several years later, after I finished high school and decided to follow Chemistry studies in France. During my first semester, I enrolled in an elective programming course where I first experienced the joy of telling a computer what to do. As the weeks went by, I found myself spending more and more time coding rather than nurturing my bonds with Chemistry. A few months later, it was clear that I needed to stop fooling myself. It was then that I decided to leave the country of love and cheese to become a full-time nerd somewhere else. A place where my mediocre French pronunciation was not an issue.
+                And here I am - 2 years later - walking around that flat earthed country half-way through my IT degree.
+                  <p> </p>
+    </div>
+</div>
+     </section>
+     <!-- #about me area -->
+
+     <!--skills-->
+     <section class="skillbg-light" id="skills"  style="padding: 40px 10%;">
+        <div class="ability py-3">
+            <h5 class="text-uppercase font-staat font-size-34 ">What are my stenghts?</h5>
+        </div>
+        <div class="row">
+          <img src="./assets/pathh.jpg" alt="" style="display: block;margin:auto" class="skillsimg">
+           
+         
+        
+    </section>
+     <!--#skills-->
+
+     <!--services-->
+     <section class="services-area" id="services" style="padding: 40px 10%;">
+        <div class="do py-3">
+            <h1 class="text-uppercase font-staat font-size-34">What can I bring to the table?</h1>
+        </div>
+        <div class="row" >
+            <div class="col-md-4 col-sm-6 mb-4 text-center" style="display: flex">
+                <div class="panel border p-4" style="min-height:294px ;">
+                    <span class="icon text-secondary"><i class="fas fa-laptop fa-3x" style="color: white;"></i></span>
+                    <h4 class="font-ram py-4">Web Design</h4>
+                    <p class="font-inter font-size-16 ">
+                       I have skills both in front-end and back-end, but I'd always prefer to work on the front-end part of the website.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 mb-4 text-center" style="display: flex">
+                <div class="panel border p-4" style="min-height:294px ;">
+                    <span class="icon text-secondary"><i class="fas fa-mobile-alt fa-3x" style="color: white;"></i></span>
+                    <h4 class="font-ram py-4">Android and PWA Development</h4>
+                    <p class="font-inter font-size-16 ">
+                    During my specialization I have built 4 applications, 2 of which in Android and 2 PWA ones.
+                    </p>
+                </div>
+            </div> 
+            <div class="col-md-4 col-sm-6 mb-4 text-center"  style="display: flex">
+                <div class="panel border p-4" style="min-height:294px ;">
+                    <span class="icon text-secondary"><i class="fas fa-chart-line fa-3x" style="color: white;"></i></span>
+                    <h4 class="font-ram py-4">UX/UI</h4>
+                    <p class="font-inter font-size-16 ">
+                     I have followed UX and UI workshops and I have build my Smart Mobile Specialization project based on them.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 mb-4 text-center" style="display: flex">
+                <div class="panel border p-4" style="min-height:294px ;">
+                    <span class="icon text-secondary"><i class="fas fa-laptop-code fa-3x" style="color: white;"></i></span>
+                    <h4 class="font-ram py-4">C#/.NET</h4>
+                    <p class="font-inter font-size-16 ">
+                        Having fallowed multiple courses in C# and .NET both in Softuni(Sofia) and Fontys(Eindhoven), I have aquired a deep knowledge in that area.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 mb-4 text-center" style="display: flex">
+                <div class="panel border p-4" style="min-height:294px ;" >
+                    <span class="icon text-secondary"><i class="fas fa-palette fa-3x" style="color: white;"></i></span>
+                    <h4 class="font-ram py-4">Graphic Design</h4>
+                    <p class="font-inter font-size-16 ">
+                       Most of my skills in this are are aquired by my curiousity and creative nature and the many hours dedicaded on trying to finish different artworks.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 mb-4 text-center" style="display: flex">
+                <div class="panel border p-4"  style="min-height:294px ;">
+                    <span class="icon text-secondary"><i class="fas fa-database fa-3x" style="color: white;"></i></span>
+                    <h4 class="font-ram py-4">Database</h4>
+                    <p class="font-inter font-size-16">
+                      Although It is not the most interesting thing for me to do, database modification is a skill that Fontys has provided me with.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+     <!--#services-->
+
+     <!--work-->
+     <section class="work" id="projects" style="padding: 0px 10%;">
+         <div class="py-3">
+            <h5 class="text-uppercase font-staat font-size-34 ">Selected projects</h5>
+           
+         </div>
+             <div class="row m-0 py-4">
+             
+            <div class="col-sm-4 pl-0 " >
+                <img src="./assets/smartmockups_k9356zhj.png" alt="work1"style="width: 260px;height:520px">
+            </div> 
+                <div class="col-sm-4 pl-0 "  style="display: flex">
+                <img src="./assets/smartmockups_k934sh6c.png" alt="work1"style="width: 260px;height:520px">
+            </div> 
+       
+            <div class="col-sm-4 pl-0" >
+                <div style="margin: 50% 0px;">
+          <H1 class="font-staat font-size-34">WeRollin'</H1>
+           <p>An app that connects skaters form the neighbourhood as well as people from all over the world. There is no need to feel like a noob as a beginner skater anymore. Get your WeRollin' App and find your local skating community.</p>
+           <a href="https://github.com/kalina957">
+            <span style="color: #20bf6b;">Source code</span> 
+            <i class="fab fa-github" style="color: #20bf6b; height: 20px;width: 20px;"></i>
+          </a>
+        </div> 
+    </div>
+             </div>
+             <div class="row m-0 py-4 ">
+                <div class="col-sm-4 pl-0">
+                    <div style="margin: 50% 0px;">
+              <H1 class="font-staat font-size-34">PartyBuddies</H1>
+               <p>Our app helps you meet cool people who share your unique music taste. Whether you want to go on an exciting first date, or you just want to go to that concert and you don't have a buddy for it, PartyBuddies is the app to go for.</p>
+               <a href="https://github.com/kalina957">
+               <span style="color: #20bf6b;">Source code</span> 
+                <i class="fab fa-github" style="color: #20bf6b; height: 20px;width: 20px;"></i>
+              </a>
+            </div> 
+        </div>
+             <div class="col-sm-4 pl-0 ">
+                <img src="./assets/smartmockups_k9e4zx3t.png" alt="work1"style="width: 280px;height:560px">
+            </div> 
+          
+            <div class="col-sm-4 pl-0 ">
+                <img src="./assets/smartmockups_k9eabve6.png" alt="work1"style="width: 280px;height:560px">
+            </div> 
+             </div>
+             
+             <div class="row m-0 py-4 ">
+              
+             <div class="col-sm-4 pl-0 ">
+                <img src="./assets/smartmockups_k9ki5n98.png" alt="work1"style="width: 280px;height:560px">
+            </div> 
+          
+            <div class="col-sm-4 pl-0 ">
+                <img src="./assets/smartmockups_k9ij4kac.png" alt="work1"style="width: 280px;height:560px">
+            </div> 
+            <div class="col-sm-4 pl-0">
+                <div style="margin: 50% 0px;">
+          <H1 class="font-staat font-size-34">FitPhone</H1>
+           <p>Our client wanted an app that brings awareness of the amount of time that a person spends on his phone on a daily bases and the target group was Fontys HealthCare students. We came up with the idea of a character which reflects a person's emotional state based on the sleeping quality of the user. This has been done based on an algorithm that we came up with a professional in the area.</p>
+           <a href="https://github.com/kalina957" style="width: 30px;height: 30px;">
+            
+            <span style="color: #20bf6b;">Source code</span> 
+           
+           <i class="fab fa-github" style="color: #20bf6b; height: 40px;width: 40px;"></i>
+          </a>
+        </div> 
+    </div>
+             </div>
+             
+     </section>
+     <!--#work-->
+
+     <!--illustrations-->    <section class="skillbg-light" id="illustrations"  style="padding: 40px 10%;">
+      <div class="py-3 px-2">
+        <h1 class="text-uppercase font-staat font-size-34 ">What do I do apart from coding?</h1>
+    </div>
+        <div class="illustrations-container">
+  <div>
+      <img src="./assets/drawing1.jpg" class="illustrationImg" alt="">
+  </div>
+  <div>
+      <img src="./assets/drawing2.jpg" class="illustrationImg" alt="">
+    </div>
+    <div>
+        <img src="./assets/girl11.jpg" class="illustrationImg" alt="">
+    </div>
+</div>
+    </section>
+    <!--#illustrations-->
+    
+
+     <footer id="footer" class=""  style="padding: 0px 10%;">
+<div id="contact" style="color: white; background-color: black;">
+    <div class="py-3 px-2">
+        <h1 class="text-uppercase font-staat font-size-34">Contact Me</h1>
+    </div>
+    <div class="form-contact contact-row row py-5">
+        <div class="about-text col-sm-6">
+            <p class="contact-txt font-inter w-70 font-size-27 ">I'm always interested in making cool stuff! If you have a project that you want to get started, think you need my help with something or just fancy saying hey, then get in touch.</p>            
+        </div>
+        <div class="col-sm-6 py-3 px-3">
+        <form class="contact-form"  method="POST" action="contactform.php">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 mb-4 text-center">
+                    <input type="text" class="contact txt-area-contact form-control" placeholder="Name" name="name" id="">
+                </div>
+                <div class="col-md-6 col-sm-6 mb-4 text-center">
+                    <input type="email" class="contact txt-area-contact form-control" placeholder="Email" name="email" id=""></div>
+            </div>
+            <textarea cols="30" rows="4" class="txt-area-contact form-control my-4" style="resize: none;" name="message" placeholder="Message"></textarea>
+            <button style="background-color: black;border: 0px ;" name="button" type="submit">
+            <div class="button_cont" style="align-content: center;"><a class="example_d" target="_blank" rel="nofollow noopener">Send Message</a></div>
+        </button>
+        </form>
+        </div>
+    </div>
+</div>
+   <div class="social-icons" style="color:white;display: flex;">
+
+                   <div class="social-icons">
+                    <ul>
+                        <li>
+                          <a href="https://github.com/kalina957">
+                            <i class="fab fa-github"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.linkedin.com/in/kalina-atanasova-3b013217a/">
+                            <i class="fab fa-linkedin"></i>
+                          </a>
+                          </li>
+                        <li>
+                          <a href="https://www.instagram.com/kalinski.at/" >
+                            <i class="fab fa-instagram"></i>
+                          </a>
+                          
+                        </li>
+                      </ul>
+                    </div>
+     </footer>
+
+</div>
+</div>
+</div>
+<!-- #header area -->
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
+  <script src="./vendor/typed/typed.min.js"></script>
+  <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+  <script src="https://kit.fontawesome.com/d7df53ae7d.js" crossorigin="anonymous"></script>
+    <script src="index.js"></script>
+</body>
+</html>
